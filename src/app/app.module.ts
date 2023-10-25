3f39c5c61a801a6141f171b171b8391934e47517
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-10-24 21:03:23 -0500

    [+] Hero: Define Interface [+] HeroesComponent: Estructura vista y funcionalidad del Componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Counter } from './counter/counter';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Counter
+    Counter,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/interfaces/hero.ts
@@ -0,0 +1,6 @@
+export interface Hero {
+    id: Number;
+    humanName?: String;
+    realName: String;
+    alterego?: String;
+}
