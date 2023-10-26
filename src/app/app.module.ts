17a8d59e6d5f56962b9609193cac9d6355e85f82
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-10-25 20:49:14 -0500

    [+] ng generate component heroes/hero-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { Counter } from './counter/counter';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Counter,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,5 +1,10 @@
 import { Hero } from "./interfaces/hero";
 
 export const HEROES: Hero[] = [
-    
+    { id: 12, realName: 'Bruce Wayne', alterego: 'Batman' },
+    { id: 13, realName: 'Bruce Banner', alterego: 'Hulk'  },
+    { id: 14, realName: 'Peter Parker', alterego: 'Spiderman' },
+    { id: 15, humanName: 'Clark Kent', realName: 'Kal-El', alterego: 'Superman' },
+    { id: 16, realName: 'Logan', alterego: 'Wolverine' },
+    { id: 17, humanName: 'Diana Prince', realName: 'Diana de Themyscira', alterego: 'Wonder Woman' }
 ];
